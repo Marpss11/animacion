c561793c7230449e26b4d9b8b9df60e1abc3ed9e
Marpss11 <mariamparedes23@gmail.com>
2025-05-19 07:27:06 +0000

    cambios en codigo

--- animacion.sh
+++ animacion.sh
@@ -13,59 +13,41 @@ REINICIAR='\033[0m'
 columnas=$(tput cols)
 
 # Número de filas en la pantalla
-filas=$(tput líneas)
+filas=$(tput lines)
 
 # Crear un array para almacenar las posiciones de las cascadas
-declarar -a columna
-para ((i=0; i<columnas; i++)); hacer
+declare -a columna
+for ((i=0; i<columnas; i++)); do
     columna[$i]=$((RANDOM % filas)) # Posición inicial aleatoria para cada columna
-hecho
+done
 
 # Función para generar un carácter aleatorio
-carácter aleatorio() {
-    caracteres locales=("A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z" "a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z" "1" "2" "3" "4" "5" "6" "7" "8" "9" "0" "!" "@" "#" "$" "%" "^" "&" "*" "(" ")")
+caracter_aleatorio() {
+    local caracteres=("A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z" "a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z" "1" "2" "3" "4" "5" "6" "7" "8" "9" "0" "!" "@" "#" "$" "%" "^" "&" "*" "(" ")")
     echo "${caracteres[$RANDOM % ${#caracteres[@]}]}"
 }
 
 # Iniciar la animación
-mientras sea cierto; hacer
-    # Para cada columna
-    para ((i=0; i<columnas; i++)); hacer
-        # Elegir un color aleatorio para cada carácter
-        color=$((ALEATORIO % 6))
-        caso $color en
-            0) código_color=$ROJO ;;
-            1) código_color=$VERDE ;;
-            2) color_code=$AMARILLO ;;
-            3) código_color=$AZUL ;;
-            4) código_color=$MAGENTA ;;
-            5) código_color=$CYAN ;;
+while true; do
+    for ((i=0; i<columnas; i++)); do
+        color=$((RANDOM % 6))
+        case $color in
+            0) codigo_color=$ROJO ;;
+            1) codigo_color=$VERDE ;;
+            2) codigo_color=$AMARILLO ;;
+            3) codigo_color=$AZUL ;;
+            4) codigo_color=$MAGENTA ;;
+            5) codigo_color=$CIAN ;;
         esac
 
-        # Mueva el cursor a la posición correspondiente
-        tput taza ${columna[$i]} $i
+        tput cup ${columna[$i]} $i
+        echo -ne "${codigo_color}$(caracter_aleatorio)${REINICIAR}"
 
-        # Imprimir carácter aleatorio en la fila correspondiente con un color aleatorio
-        echo -ne "${código_de_color}$(carácter_aleatorio)${RESET}"
-
-        # Actualizar la posición de la cascada
         ((columna[$i]++))
-        if ((columna[$i] >= filas )); entonces
-            columna[$i]=0 # Reiniciar la cascada cuando llega al final
+        if ((columna[$i] >= filas)); then
+            columna[$i]=0
         fi
-    hecho
+    done
 
-    # Pausar para hacer la animación fluida
-    dormir 0.05
-hecho
-
-    # Pausar para hacer la animación fluida
-    dormir 0.1
-hecho
-    # Pausar para hacer la animación fluida
-    dormir 0.1
-hecho
-
-    # Pausar para hacer la animación fluida
-    dormir 0.1
-hecho
+    sleep 0.05
+done
